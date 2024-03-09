661662c1acc650876501058d6db602f796864cd6
ranjan3nov <ranjan3nov@gmail.com>
2024-03-09 16:04:23 +0530

    erro view added on the login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,17 +14,22 @@
                     <div class="mb-3">
                         <label for="email" class="form-label">Email address</label>
                         <input type="email" class="form-control" id="email" name="email">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password">
-                    </div>
-                    <div class="mb-3 form-check">
-                        <a href="{{ route('register') }}" class="text-white">Not Registered ?? Click to Registerd</a>
-                    </div>
-                    <button type="submit" class="btn btn-primary w-100">Submit</button>
-                </form>
-            </div>
+                        @if (session('error'))
+                            <span class="text-danger">
+                                <strong>*{{ session('error') }}</strong>
+                            </span>
+                        @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <div class="mb-3 form-check">
+                    <a href="{{ route('register') }}" class="text-white">Not Registered ?? Click to Registerd</a>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Submit</button>
+            </form>
         </div>
     </div>
+</div>
 @endsection
